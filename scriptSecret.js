099a1ac643820d2293a9c8a8fda4a5d20dcb39a7
Kamaluddin Arsyad Fadllillah <102853169+d-arsya@users.noreply.github.com>
2022-06-26 11:45:12 +0700

    Update scriptSecret.js

--- scriptSecret.js
+++ scriptSecret.js
@@ -4,24 +4,19 @@ const form = document.forms['secret-message'];
 const reLaunch = document.querySelector(".reLaunch");
 const finish = document.querySelector(".finish");
 const messageForm = document.querySelector(".messageForm");
-let menit = Math.floor(Math.random() * 59) + 1;
 const tip = document.querySelector(".minutes");
+const loading = document.querySelector(".btn");
+const jamKirim = document.querySelector("#time");
+const jam = document.querySelector('.jam');
+let menit = Math.floor(Math.random() * 59) + 1;
 tip.innerHTML=menit;
-var jam = document.querySelector('.jam');
 
-function time() {
-  var d = new Date();
-  var s = d.getSeconds();
-  var m = d.getMinutes();
-  var h = d.getHours();
-  jam.textContent = 
-    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
-}
-
-setInterval(time, 1000);
+setInterval(realTime, 1000);
 
 form.addEventListener('submit', e => {
+    loading.classList.toggle("hide");
     e.preventDefault()
+    jamKirim.value=jam.textContent;
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
         console.log('Success!', response);
@@ -31,21 +26,32 @@ form.addEventListener('submit', e => {
         console.error('Error!', error.message)
     })
 })
+
 finish.addEventListener("click",function(){
     document.querySelector(".container").style.display="none";   
 })
+
 reLaunch.addEventListener("click",function(){
     normal();
 })
+
+function realTime() {
+    let d = new Date();
+    let s = d.getSeconds();
+    let m = d.getMinutes();
+    let h = d.getHours();
+    jam.textContent = 
+    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+}
 function normal(){
+    loading.classList.toggle("hide");
     reLaunch.style.display="none";
     finish.style.display="none";
     messageForm.style.display="block";
     form.reset();
 }
 function selesai(){
-    reLaunch.style.display="inline-block";
-    finish.style.display="inline-block";
+    reLaunch.style.display="block";
+    finish.style.display="block";
     messageForm.style.display="none";
-    form.reset();
 }
