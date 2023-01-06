812bb60f4afc191745d73dd1668f570eb6e460b4
Kamaluddin Arsyad Fadllillah <102853169+d-arsya@users.noreply.github.com>
2023-01-07 06:35:39 +0700

    Create kuliahScript.js

---
+++ kuliahScript.js
@@ -0,0 +1,13 @@
+const articles = document.querySelector("article");
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+  if(xhr.readyState==4 && xhr.status==200){
+    let data = JSON.parse(this.responseText).article;
+    for(i=0 ; i<= data.length ; i++){
+      articles.innerHTML += `<a target="_self" href="article/${data[i].link}" class="articleContents"><img src="${data[i].image}" alt=""><h3>${data[i].tagLine}</h3><h4 class="date">${data[i].dateCreated}</h4></a>`
+    }
+  }
+}
+
+xhr.open("GET", "http://d-arsya.epizy.com/acmtv/kuliah/script/db.json", true)
+xhr.send()
